c0896d003264a4b4047684cf944f8a8d0787606e
Trevor Williams <tmw7991@gmail.com>
2024-01-25 09:13:55 -0700

    add noise

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import WebGL from "three/addons/capabilities/WebGL.js";
 main();
 
 function main() {
+  const canvas = document.querySelector("#c");
+  if (!canvas) return;
+
   const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x000000);
+
+  // Camera setup
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -13,16 +19,19 @@ function main() {
     25
   );
 
-  const canvas = document.querySelector("#c");
-  if (!canvas) return;
+  const xRotationDeg = -65;
+  const xRotationRad = (xRotationDeg * Math.PI) / 180;
+  camera.rotateX(xRotationRad);
+  camera.position.y = 4;
+  camera.position.z = -1;
 
   const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
   // Grid parameters
-  const gridSize = window.innerHeight / 5;
+  const gridSize = window.innerHeight / 4;
   const aspectRatio = window.innerWidth / window.innerHeight;
-  const spacing = 0.1;
-  const xFactor = aspectRatio;
+  const xFactor = (aspectRatio / Math.sin(-xRotationRad)) * 1.5;
+  const spacing = 0.05;
   const offset = -gridSize * spacing * xFactor * 0.5;
 
   // Points geometry
@@ -38,53 +47,102 @@ function main() {
     new THREE.Float32BufferAttribute(positions, 3)
   );
 
-  // Points material using a Shader
   const pointsMaterial = new THREE.ShaderMaterial({
     uniforms: {
       time: { value: 0.0 },
-      gridSize: { value: 100 }, // Assuming gridSize is your actual grid size
-      spacing: { value: 0.2 }, // Assuming spacing is your actual spacing value
+      pointSize: { value: 2.0 },
+      noiseAmp1: { value: 0.35 }, // Adjust as needed
+      noiseFreq1: { value: 1.0 }, // Adjust as needed
+      spdModifier1: { value: 0.5 }, // Adjust as needed
+      noiseAmp2: { value: 0.25 }, // Adjust as needed
+      noiseFreq2: { value: 1.0 }, // Adjust as needed
+      spdModifier2: { value: 0.5 }, // Adjust as needed
+      gridSize: { value: gridSize },
+      spacing: { value: spacing },
+      aspectRatio: { value: aspectRatio },
     },
     vertexShader: `
+    #define PI 3.14159265359
+
     uniform float time;
+    uniform float pointSize;
+    uniform float noiseAmp1;
+    uniform float noiseFreq1;
+    uniform float spdModifier1;
+    uniform float noiseAmp2;
+    uniform float noiseFreq2;
+    uniform float spdModifier2;
+
     varying float vX;
     varying float vZ;
 
+    float random (in vec2 st) {
+        return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+    }
+
+    float noise (in vec2 st) {
+        vec2 i = floor(st);
+        vec2 f = fract(st);
+
+        float a = random(i);
+        float b = random(i + vec2(1.0, 0.0));
+        float c = random(i + vec2(0.0, 1.0));
+        float d = random(i + vec2(1.0, 1.0));
+
+        vec2 u = f*f*(3.0-2.0*f);
+
+        return mix(a, b, u.x) + (c - a)* u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+    }
+
+    mat2 rotate2d(float angle){
+        return mat2(cos(angle),-sin(angle), sin(angle),cos(angle));
+    }
+
     void main() {
+        gl_PointSize = pointSize;
+
         vec3 pos = position;
-        pos.y += sin(time + pos.x) * 0.25;
-        pos.y += sin(time * 0.5 + pos.z) * 0.15;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
-        gl_PointSize = 3.0;
+        pos.y += noise(pos.xz * noiseFreq1 + time * spdModifier1) * noiseAmp1;
+        pos.y += noise(rotate2d(PI / 4.) * pos.zx * noiseFreq2 - time * spdModifier2 * 0.6) * noiseAmp2;
+
+        vec4 mvm = modelViewMatrix * vec4(pos, 1.0);
+        gl_Position = projectionMatrix * mvm;
 
         vX = pos.x;
         vZ = pos.z;
     }
     `,
+    // fragmentShader: `
+    // uniform float gridSize;
+    // uniform float spacing;
+    // uniform float aspectRatio;
+
+    // varying float vX;
+    // varying float vZ;
+
+    // void main() {
+    //     // Calculate the range of x and z
+    //     float maxX = gridSize * spacing / 2.0;
+    //     float maxZ = gridSize * aspectRatio * spacing / 2.0;
+
+    //     // Normalize the x and z values based on their maximum values
+    //     float normalizedX = (vX + maxX) / (2.0 * maxX);
+    //     float normalizedZ = (vZ + maxZ) / (2.0 * maxZ);
+
+    //     // Create the diagonal gradient based on normalized x and z
+    //     float normalizedValue = (normalizedX + normalizedZ) / 2.0;
+    //     normalizedValue = clamp(normalizedValue, 0.0, 1.0);
+
+    //     vec3 blue = vec3(0.0, 0.0, 1.0);
+    //     vec3 teal = vec3(0.0, 1.0, 1.0);
+
+    //     vec3 color = mix(blue, teal, normalizedValue);
+    //     gl_FragColor = vec4(color, 1.0);
+    // }
+    // `,
     fragmentShader: `
-    uniform float gridSize;
-    uniform float spacing;
-    varying float vX;
-    varying float vZ;
-
     void main() {
-        // Calculate the range of x and z
-        float maxX = gridSize * spacing / 2.0;
-        float maxZ = gridSize * spacing / 2.0;
-
-        // Normalize the x and z values based on their maximum values
-        float normalizedX = (vX + maxX) / (2.0 * maxX);
-        float normalizedZ = (vZ + maxZ) / (2.0 * maxZ);
-
-        // Create the diagonal gradient based on normalized x and z
-        float normalizedValue = (normalizedX + normalizedZ) / 2.0;
-        normalizedValue = clamp(normalizedValue, 0.0, 1.0);
-
-        vec3 blue = vec3(0.0, 0.0, 1.0);
-        vec3 green = vec3(0.0, 1.0, 0.0);
-
-        vec3 color = mix(blue, green, normalizedValue);
-        gl_FragColor = vec4(color, 1.0);
+        gl_FragColor = vec4(0, 0.2, 1, 1.0);
     }
     `,
   });
@@ -92,11 +150,6 @@ function main() {
   // Points mesh
   const points = new THREE.Points(pointsGeometry, pointsMaterial);
   scene.add(points);
-
-  const xRotationDeg = -55;
-  camera.rotateX((xRotationDeg * Math.PI) / 180);
-  camera.position.y = 3;
-  camera.position.z = -2;
 
   function animate() {
     requestAnimationFrame(animate);
