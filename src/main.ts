ff995ae992d98752a98d705c8b679487a4af3751
Trevor Williams <tmw7991@gmail.com>
2024-01-25 13:54:09 -0700

    switch shaders, points -> plane geometry

--- src/main.ts
+++ src/main.ts
@@ -13,53 +13,43 @@ function main() {
 
   // Camera setup
   const camera = new THREE.PerspectiveCamera(
-    75,
+    60,
     window.innerWidth / window.innerHeight,
     0.1,
-    25
+    50
   );
-
-  const xRotationDeg = -65;
-  const xRotationRad = (xRotationDeg * Math.PI) / 180;
-  camera.rotateX(xRotationRad);
-  camera.position.y = 4;
-  camera.position.z = -1;
+  camera.position.set(0, 0, 3.5);
 
   const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-  // Grid parameters
-  const gridSize = window.innerHeight / 4;
-  const aspectRatio = window.innerWidth / window.innerHeight;
-  const xFactor = (aspectRatio / Math.sin(-xRotationRad)) * 1.5;
-  const spacing = 0.05;
-  const offset = -gridSize * spacing * xFactor * 0.5;
+  const planeGeometry = new THREE.PlaneGeometry(16, 8, 512, 256);
 
-  // Points geometry
-  const pointsGeometry = new THREE.BufferGeometry();
-  const positions = [];
-  for (let i = 0; i < gridSize * xFactor; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      positions.push(i * spacing + offset, 0, -j * spacing);
-    }
-  }
-  pointsGeometry.setAttribute(
-    "position",
-    new THREE.Float32BufferAttribute(positions, 3)
-  );
-
-  const pointsMaterial = new THREE.ShaderMaterial({
+  const planeMaterial = new THREE.ShaderMaterial({
     uniforms: {
       time: { value: 0.0 },
-      pointSize: { value: 2.0 },
-      noiseAmp1: { value: 0.35 }, // Adjust as needed
-      noiseFreq1: { value: 1.0 }, // Adjust as needed
-      spdModifier1: { value: 0.5 }, // Adjust as needed
-      noiseAmp2: { value: 0.25 }, // Adjust as needed
-      noiseFreq2: { value: 1.0 }, // Adjust as needed
-      spdModifier2: { value: 0.5 }, // Adjust as needed
-      gridSize: { value: gridSize },
-      spacing: { value: spacing },
-      aspectRatio: { value: aspectRatio },
+      resolution: {
+        value: {
+          x: window.innerWidth * window.devicePixelRatio,
+          y: window.innerHeight * window.devicePixelRatio,
+        },
+      },
+      pointSize: { value: 3.0 },
+      noiseAmp1: { value: 0.2 },
+      noiseFreq1: { value: 3.0 },
+      spdModifier1: { value: 1 },
+      noiseAmp2: { value: 0.1 },
+      noiseFreq2: { value: 3.0 },
+      spdModifier2: { value: 1.2 },
+
+      u_pointsize: { value: 3.0 },
+      // wave 1
+      u_noise_freq_1: { value: 3.0 },
+      u_noise_amp_1: { value: 0.2 },
+      u_spd_modifier_1: { value: 1 },
+      // wave 2
+      u_noise_freq_2: { value: 3.0 },
+      u_noise_amp_2: { value: 0.1 },
+      u_spd_modifier_2: { value: 1.2 },
     },
     vertexShader: `
     #define PI 3.14159265359
@@ -73,89 +63,83 @@ function main() {
     uniform float noiseFreq2;
     uniform float spdModifier2;
 
-    varying float vX;
-    varying float vZ;
-
+    // 2D Random
     float random (in vec2 st) {
-        return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+        return fract(sin(dot(st.xy,
+                            vec2(12.9898,78.233)))
+                    * 43758.5453123);
     }
 
+    // 2D Noise
     float noise (in vec2 st) {
         vec2 i = floor(st);
         vec2 f = fract(st);
 
+        // Four corners in 2D of a tile
         float a = random(i);
         float b = random(i + vec2(1.0, 0.0));
         float c = random(i + vec2(0.0, 1.0));
         float d = random(i + vec2(1.0, 1.0));
 
-        vec2 u = f*f*(3.0-2.0*f);
+        // Smooth Interpolation
 
-        return mix(a, b, u.x) + (c - a)* u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+        // Cubic Hermine Curve.  Same as SmoothStep()
+        vec2 u = f*f*(3.0-2.0*f);
+        // u = smoothstep(0.,1.,f);
+
+        // Mix 4 coorners percentages
+        return mix(a, b, u.x) +
+                (c - a)* u.y * (1.0 - u.x) +
+                (d - b) * u.x * u.y;
     }
 
     mat2 rotate2d(float angle){
-        return mat2(cos(angle),-sin(angle), sin(angle),cos(angle));
+        return mat2(cos(angle),-sin(angle),
+                  sin(angle),cos(angle));
     }
 
     void main() {
-        gl_PointSize = pointSize;
+      gl_PointSize = pointSize;
 
-        vec3 pos = position;
-        pos.y += noise(pos.xz * noiseFreq1 + time * spdModifier1) * noiseAmp1;
-        pos.y += noise(rotate2d(PI / 4.) * pos.zx * noiseFreq2 - time * spdModifier2 * 0.6) * noiseAmp2;
+      vec3 pos = position;
 
-        vec4 mvm = modelViewMatrix * vec4(pos, 1.0);
-        gl_Position = projectionMatrix * mvm;
+      // pos.xy is the original 2D dimension of the plane coordinates
+      pos.z += noise(pos.xy * noiseFreq1 + time * spdModifier1) * noiseAmp1;
+      // add noise layering
+      // minus time makes the second layer of wave goes the other direction
+      pos.z += noise(rotate2d(PI / 4.) * pos.yx * noiseFreq2 - time * spdModifier2 * 0.6) * noiseAmp2;
 
-        vX = pos.x;
-        vZ = pos.z;
+      vec4 mvm = modelViewMatrix * vec4(pos, 1.0);
+      gl_Position = projectionMatrix * mvm;
     }
     `,
-    // fragmentShader: `
-    // uniform float gridSize;
-    // uniform float spacing;
-    // uniform float aspectRatio;
-
-    // varying float vX;
-    // varying float vZ;
-
-    // void main() {
-    //     // Calculate the range of x and z
-    //     float maxX = gridSize * spacing / 2.0;
-    //     float maxZ = gridSize * aspectRatio * spacing / 2.0;
-
-    //     // Normalize the x and z values based on their maximum values
-    //     float normalizedX = (vX + maxX) / (2.0 * maxX);
-    //     float normalizedZ = (vZ + maxZ) / (2.0 * maxZ);
-
-    //     // Create the diagonal gradient based on normalized x and z
-    //     float normalizedValue = (normalizedX + normalizedZ) / 2.0;
-    //     normalizedValue = clamp(normalizedValue, 0.0, 1.0);
-
-    //     vec3 blue = vec3(0.0, 0.0, 1.0);
-    //     vec3 teal = vec3(0.0, 1.0, 1.0);
-
-    //     vec3 color = mix(blue, teal, normalizedValue);
-    //     gl_FragColor = vec4(color, 1.0);
-    // }
-    // `,
     fragmentShader: `
+    #ifdef GL_ES
+    precision mediump float;
+    #endif
+
+    #define PI 3.14159265359
+    #define TWO_PI 6.28318530718
+    
+    uniform vec2 resolution;
+
     void main() {
-        gl_FragColor = vec4(0, 0.2, 1, 1.0);
+      vec2 st = gl_FragCoord.xy/resolution.xy;
+
+      gl_FragColor = vec4(vec3(0.0, st),1.0);
     }
     `,
   });
 
   // Points mesh
-  const points = new THREE.Points(pointsGeometry, pointsMaterial);
+  const points = new THREE.Points(planeGeometry, planeMaterial);
+  points.rotation.x = -3.1415 / 8;
   scene.add(points);
 
   function animate() {
     requestAnimationFrame(animate);
 
-    // Update time uniform
-    pointsMaterial.uniforms.time.value = performance.now() * 0.002;
+    planeMaterial.uniforms.time.value = performance.now() * 0.001;
 
     renderer.render(scene, camera);
 
